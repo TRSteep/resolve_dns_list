19107dd89d8b8e11f9733264db21736b56dc0783
trsteep <trsteep@yandex.ru>
2023-01-11 13:34:54 +0300

    Add path

--- resolve_dns_list.ps1
+++ resolve_dns_list.ps1
@@ -1,10 +1,20 @@
-﻿Set-ExecutionPolicy Bypass -Scope Process
+﻿#ByPass Restriction
+Set-ExecutionPolicy Bypass -Scope Process
 
+# Path
+$Path = "C:\Media\dev\ps\resolve_dns_list"
+
+
+# IP List list or file
 #$IPList = @('172.16.82.57','172.16.81.95','10.20.26.13')
-$IPList = Get-Content C:\Media\dev\ps\resolve_dns_list_ip.txt -Encoding UTF8
-$ServerList = @('172.16.20.11','172.16.20.12')
+$IPList = Get-Content $Path\resolve_dns_list_ip.txt -Encoding UTF8
 
+# Servers lists
+$ServerList = @('172.16.20.11','172.16.20.12')
+# Result Data
 $FinalResult = @()
+
+# Collect Data
 foreach ($IP in $IPList) {
     $tempObj = "" | Select-Object Name, IPAddress, Status, ErrorMessage
     try {
